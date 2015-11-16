2241424ec821f1204ee285c9fb3695793a25906a
Ilya Bazylchuk <ilya.bazylchuk@gmail.com>
2015-11-16 14:50:29 +0300

    Update ruby to 2.2.3 Update rails to 4.1.14 Create new api daily report

--- app/controllers/api/v1/stats_controller.rb
+++ app/controllers/api/v1/stats_controller.rb
@@ -23,6 +23,29 @@ class Api::V1::StatsController < ApplicationController
     end
   end
 
+  def daily_report
+    date = Date.parse(params[:date]) rescue Date.yesterday
+    count_problems = @app.problems.where(:updated_at.gt => date.beginning_of_day).where(:updated_at.lt => date.end_of_day).count
+    count_times = @app.problems.where(:updated_at.gt => Date.yesterday.beginning_of_day).where(:updated_at.lt => Date.yesterday.end_of_day).sum(&:notices_count)
+    count_resolved = @app.problems.where(:resolved_at.gt => date.beginning_of_day).where(:resolved_at.lt => date.end_of_day).count
+    count_all_errors = @app.unresolved_count
+
+    stats = {
+      :name => @app.name,
+      :id => @app.id,
+      :date => date.strftime("%m/%d/%y"),
+      :daily_problems_count => count_problems,
+      :daily_error_times => count_times,
+      :daily_resolved => count_resolved,
+      :total_problems => count_all_errors,
+      :unresolved_errors => @app.unresolved_count
+    }
+
+    respond_to do |format|
+      format.any(:html, :json) { render :json => JSON.dump(stats) } # render JSON if no extension specified on path
+      format.xml  { render :xml  => stats }
+    end
+  end
 
   protected
 
